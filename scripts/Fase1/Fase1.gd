4bf3b7a0fed88de74205ce243a93b9b5b29eb4d6
laraesquivel <laraesquivelsantos@gmail.com>
2023-06-16 23:04:49 -0300

    HTPP funciona como GET

--- scripts/Fase1/Fase1.gd
+++ scripts/Fase1/Fase1.gd
@@ -15,10 +15,32 @@ enum PLAYER_STATE {
 
 var player_turn = PLAYER_STATE.Wait
 
+var data = {
+	"coords":"(3,3)",
+	"expression":"2x",
+	"dominio_min":-3,
+	"dominio_max":3
+}
+
 
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
+	$HTTPRequest.connect("request_completed", self, "_on_HTTPRequest_request_completed")
+	
+	var headers = PoolStringArray()
+	headers.append("Content-Type: application/json")
+	var body_str = to_json(data)
+	var caminho_absoluto = ProjectSettings.globalize_path("res://")
+	print(caminho_absoluto.replace("math_magic/",""))
+	caminho_absoluto += "pontos.csv/"
+	print(caminho_absoluto)
+	
+	$HTTPRequest.request("http://127.0.0.1:5000/getPoints2/x/3/-3", headers, true, 0)
+
 	set_player_turn()
+
+
 
 func _input(event):
 	if event is InputEventKey and event.scancode == KEY_ESCAPE and event.pressed:
@@ -54,3 +76,15 @@ func get_player_turn():
 func on_timer_out():
 	#Temporizador expirou
 	print("temporizador expirou")
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+		if response_code == 200:
+			var response = body.get_string_from_utf8()
+			print(response)
+			print(headers)
+			
+		else:
+			print("An error occurred in the HTTP request.")
+			
+	

32eca1af1678c30bb74428002bcc0db66c75d1a5
Paulo Queiroz <paulocarvalho.pq@gmail.com>
2023-06-16 22:04:10 -0300

    add: cena magic que faz o caminho do tiro

--- scripts/GlobalObjects/SpellShot.gd
+++ scripts/GlobalObjects/SpellShot.gd
@@ -15,10 +15,10 @@ func set_diraction(dir : Vector2):
 	rotation_degrees = rad2deg(direction.angle_to(Vector2(1,0)))
 	direction.x *= -1
 		
-func _physics_process(delta : float) -> void:
-	velocity.x = SPEED * delta * direction.x
-	velocity.y = SPEED * delta * direction.y
-	translate(velocity)
+#func _physics_process(delta : float) -> void:
+#	velocity.x = SPEED * delta * direction.x
+#	velocity.y = SPEED * delta * direction.y
+#	translate(velocity)
 
 func _on_SpellShot_body_entered(body : Node):
 	if(body.is_in_group("walls")):
@@ -26,6 +26,6 @@ func _on_SpellShot_body_entered(body : Node):
 		Global.emit_hit()
 		queue_free()
 
+#func _on_VisibilityNotifier2D_screen_exited():
+#	queue_free()
 
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()

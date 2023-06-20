a712f7dcca6348dc677c0de47e0dffc7fe400c2b
Paulo Queiroz <paulocarvalho.pq@gmail.com>
2023-06-20 20:16:40 -0300

    add: logica do tiro

--- Cenas/GreenW.gd
+++ Cenas/GreenW.gd
@@ -15,13 +15,26 @@ var newPosition = position
 var this_wizard
 
 func _ready():
+	$HTTPA.connect("request_completed", self, "_on_HTTPRequest_request_completed")
 	print("KinematicBody lendo estado")
+	
 	var daddy = get_parent()
 	print(daddy.get_player_turn())
 	this_wizard = get_parent().PLAYER_STATE.GREEN
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func generete_query_string(math_expression,valor):
+	var BASE_STR = "http://127.0.0.1:5000/getPoints2/"
+	BASE_STR += math_expression + "/"
+	print(BASE_STR)
+	if valor == 1:
+		BASE_STR += "100/0"
+	elif valor==-1:
+		BASE_STR +="0/-100"
+	print(BASE_STR)
+	return BASE_STR
 
 
 func get_input():
@@ -50,12 +63,14 @@ func get_input():
 		#Verifica se foi solicitado o tiro
 		if Input.is_action_just_pressed("shot"):
 			#get_parent().set_player_turn()
-			get_parent().shot_runing = 1
-			var shotInstance = SHOT.instance()
-			get_parent().add_child(shotInstance)
-			shotInstance.z_index = -1
-			shotInstance.position = $spellPoint.global_position
-			#shotInstance.set_diraction(shotDirection)
+			var x = (get_parent()).direction_shot
+			var texto = (($"../LineEdit").get_text()).replace(" ","")
+			var query_string = generete_query_string(texto,x)
+			var headers = PoolStringArray()
+			headers.append("Content-Type: application/json")
+			$HTTPA.request(query_string, headers, true, 0)
+	
+
 			
 		velocity *= speed
 	
@@ -68,11 +83,52 @@ func _physics_process(delta):
 
 func kill():
 	get_parent().kill(this_wizard)
-	$Green.self_modulate = Color("404040")
+	$Azul.self_modulate = Color("404040")
 	$CollisionShape2DAzul.queue_free()
 	z_index = -2
-
+	
 func spawn(spawnPoint : Vector2):
 	position = spawnPoint
 	oldPosition = position
 	newPosition = position
+
+
+func _on_HTTPAZUL_request_completed(result, response_code, headers, body):
+	if response_code == 200:
+			var response = body.get_string_from_utf8()
+			#print(response)
+			print(headers)
+			var points = str(response).split(',')
+			var vector_array = []
+			
+			
+			for coords in points:
+				var aux = coords.split("/")
+				if len(aux) ==2:
+					var x = (float(aux[0])/100) * 640
+					var y = ((0-(float(aux[1])))/100) * 640
+					var v = Vector2(x,y)
+					vector_array.append(v)
+			print(vector_array)
+				
+			get_parent().shot_runing = 1
+			var shotInstance = SHOT.instance()
+			shotInstance.z_index = -1
+			shotInstance.position = $spellPoint.global_position 
+			get_parent().add_child(shotInstance)
+			
+			#print(response['result'])
+			
+			#if points['status'] != 500:
+			#	var response_list = points['result']
+			#	for t in response_list:
+			#		print(t)
+			#	get_parent().shot_runing = 1
+			#	var shotInstance = SHOT.instance()
+			#	get_parent().add_child(shotInstance)
+			#	shotInstance.z_index = -1
+			#	shotInstance.position = $spellPoint.global_position 
+			#shotInstance.set_diraction(shotDirection)
+			
+	else:
+		print("An error occurred in the HTTP request.")
--- Cenas/Objetos/magic.gd
+++ Cenas/Objetos/magic.gd
@@ -3,21 +3,28 @@ extends Path2D
 
 var speed = 400
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var label
+
+func _init(points : PoolVector2Array):
 	var newCurve = Curve2D.new()
-	for x in range(0,1000):
-		label = Label.new()
-		label.set_text(".")
-		label.set_position(Vector2(x,(50*sin(0.1*x))))
-		add_child(label)
-		#newCurve.add_point(Vector2(x,(5*sin(0.5*x))))
-		newCurve.add_point(Vector2(x,(50*sin(0.1*x))))
+	for ponint in points:
+		newCurve.add_point(ponint)
 	curve = newCurve
+
+func _ready():
+#	var label
+#	var newCurve = Curve2D.new()
+#	for x in range(0,1000):
+#		label = Label.new()
+#		label.set_text(".")
+#		label.set_position(Vector2(x,(50*sin(0.1*x))))
+#		add_child(label)
+#		#newCurve.add_point(Vector2(x,(5*sin(0.5*x))))
+#		newCurve.add_point(Vector2(x,(50*sin(0.1*x))))
+#	curve = newCurve
 	
 	var animation = $PathFollow2D/AnimationPlayer.get_animation("shot")
 	var animationTime = curve.get_baked_length() / speed
 	animation.length = animationTime 
 	var track_index = animation.find_track(NodePath(".:unit_offset"))
 	var keyId = animation.bezier_track_insert_key(track_index, animationTime, 1)#animation.track_insert_key(track_index, animationTime, 1.0)
+
